3033f7479225c8b016897082f4ff3e88ba92142b
Anton <spakantonpetr@gmail.com>
2025-03-06 19:33:12 +0200

    Лабораторна 1

---
+++ lab_1_StudentShpak.py
@@ -0,0 +1,13 @@
+# This is a simple Python script
+
+#press Shift+F10 to execute it or replace it with your code.
+#Press Double Shift to search everywhere for classes,files,test windows,actions,and settings.
+
+
+def print_hello(name):
+    #Use a breakpoint in the code line below to debug your script.
+    print(f"Hello,{name}")
+
+if __name__ =='__main__':
+    print_hello("Anton")
+
